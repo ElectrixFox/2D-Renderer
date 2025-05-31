d6a82eede969f80194ad7c64a0297a391a301944
ElectrixFox <lukecphillips2006@gmail.com>
2025-05-31 11:58:34 +0100

    Setup basic functions for render details

--- src/RenderObject.c
+++ src/RenderObject.c
@@ -1,5 +1,84 @@
 #include "RenderObject.h"
 
+RenderDetails InitialiseRenderDetails()
+{
+RenderDetails rd;   // creating the details
+
+rd.size = 0;    // setting the size to 0
+
+// allocating a small bit of memory
+rd.rid = (unsigned int*)malloc(sizeof(unsigned int));
+rd.vao = (unsigned int*)malloc(sizeof(unsigned int));
+rd.shader = (unsigned int*)malloc(sizeof(unsigned int));
+rd.texture = (unsigned int*)malloc(sizeof(unsigned int));
+
+return rd;
+}
+
+int getRenderDetailsIDIndex(RenderDetails rd, unsigned int rid)
+{
+if(rd.rid[rid] == rid)  // just in case no manipulation of the table has happened
+    return rid;
+
+for (int i = 0; i < rd.size; i++)   // simple linear search
+    if(rd.rid[i] == rid)
+        return i;
+return -1;
+}
+
+unsigned int AddRenderDetail(RenderDetails *rd, unsigned int vao, unsigned int shader, unsigned int texture)
+{
+static unsigned int id = 0; // a static incrementing counter to set the new ID as
+const unsigned int n = rd->size;
+
+// make all the arrays bigger by one to accomodate for the new element
+ExpandByOne(&rd->vao, n, sizeof(unsigned int));
+ExpandByOne(&rd->shader, n, sizeof(unsigned int));
+ExpandByOne(&rd->texture, n, sizeof(unsigned int));
+
+// setting all the new details
+rd->rid[n] = id++;  // increment the ID counter too
+rd->vao[n] = vao;
+rd->shader[n] = shader;
+rd->texture[n] = texture;
+
+return rd->rid[n];
+}
+
+void RemoveRenderDetail(RenderDetails *rd, unsigned int rid)
+{
+int index = getRenderDetailsIDIndex(*rd, rid);  // finding the ID
+
+if(index == -1)
+    return; // if the index isn't found just quit
+
+if(index == rd->size)
+    goto end;   // hehe the naughty goto
+
+// getting temporary stuff
+unsigned int tid = rd->rid[index];
+unsigned int tvao = rd->vao[index];
+unsigned int tshader = rd->shader[index];
+unsigned int ttexture = rd->texture[index];
+
+// setting the to delete to the end values
+rd->rid[index] = rd->rid[rd->size];
+rd->vao[index] = rd->vao[rd->size];
+rd->shader[index] = rd->shader[rd->size];
+rd->texture[index] = rd->texture[rd->size];
+
+// setting the end to the thing to delete
+rd->rid[rd->size] = tid;
+rd->vao[rd->size] = tvao;
+rd->shader[rd->size] = tshader;
+rd->texture[rd->size] = ttexture;
+
+end:
+rd->size--; // decrease the size so it is effectively not there
+
+// To-Do: Could add in a sort here to sort by ID in order to realign the table
+}
+
 unsigned int CreateVBO(float vertices[], unsigned int n)
 {
 unsigned int vbo;
--- src/RenderObject.h
+++ src/RenderObject.h
@@ -7,8 +7,46 @@ struct RenderDetails
     unsigned int* vao;
     unsigned int* shader;
     unsigned int* texture;
+    unsigned int size;
     };
 typedef struct RenderDetails RenderDetails;
+
+/**
+ * Initialises a new RenderDetails object
+ * 
+ * @returns A new initialised RenderDetails object
+ */
+RenderDetails InitialiseRenderDetails();
+
+/**
+ * Returns the index of the render detail
+ * 
+ * @param rd The render details object
+ * @param rid The ID of the details to find
+ * 
+ * @returns Index of ID in the details object or -1 if not found
+ */
+int getRenderDetailsIDIndex(RenderDetails rd, unsigned int rid);
+
+/**
+ * Adds a render object to the details
+ * 
+ * @param rd A pointer to the details
+ * @param vao The new vao to add
+ * @param shader The new shader to add
+ * @param texture The new texture to add
+ * 
+ * @returns The ID of the newly added detail object
+ */
+unsigned int AddRenderDetail(RenderDetails* rd, unsigned int vao, unsigned int shader, unsigned int texture);
+
+/**
+ * Removes a render object from the details
+ * 
+ * @param rd A pointer to the details
+ * @param rid The ID of the object to remove
+ */
+void RemoveRenderDetail(RenderDetails* rd, unsigned int rid);
 
 
 // optimise to store the data more compactly
